b94d39199016273bef678acd34af04c835dd9d97
Poksliun <poksliun@icloud.com>
2022-09-29 15:16:45 +0200

    Добавление первого скрипта (примера как делать не стоит).

---
+++ first_script.py
@@ -0,0 +1,12 @@
+import requests
+
+
+class TestRegistration:
+    def test_registration(self):
+        body = {"username": "test1@test.com", "password": "Password"}
+        response = requests.post("https://stores-tests-api.herokuapp.com/register", json=body)
+        assert response.status_code == 201
+        assert response.json().get('message') == 'User created successfully.'
+        assert response.json().get('uuid')
+        assert isinstance(response.json().get('uuid'), int)
+        print(response.text)
